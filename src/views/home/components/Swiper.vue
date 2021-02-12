e12b47c1f74916ef7c5d11aa2600985ed0f67e75
CalDey <2234706312@qq.com>
2021-02-12 23:29:58 +0800

    swiper

---
+++ src/views/home/components/Swiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+    <swiper-slide v-for="item of swiperList" :key="item.id">
+      <img class="swiper-img" :src="item.imgUrl" alt="">
+    </swiper-slide>
+    <div class="swiper-pagination"  slot="pagination"></div>
+  </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://imgs.qunarzz.com/vs_ceph_vcimg/6ff121fd416169b8d56c60384e3baf79.jpeg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://imgs.qunarzz.com/vs_ceph_vcimg/f7813c9431796cc32ae7b7a78447342e.jpeg'
+      },
+      {
+        id: '0002',
+        imgUrl: 'http://imgs.qunarzz.com/vs_ceph_vcimg/f03f5ac90ae59d0d9c6332a2bfd9782e.jpeg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  // >>> 样式穿透
+  .wrapper >>> .swiper-pagination-bullet-active
+      background: #fff !important
+  .wrapper
+    width: 100%
+    // height: 31.25vm
+    height: 0
+    overflow: hidden
+    padding-bottom: 31.25%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
